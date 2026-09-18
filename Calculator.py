ba953eb9a84f245c35216be87898aed5f7fa484c
Sanjana <sanjananb803@gmail.com>
2026-09-18 16:04:09 +0530

    Added the multiplication and division

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,10 @@
-a = int(input("Enter the first number:")
-b = int(input("Enter the second number:")
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
 Addition = a + b
 Subtraction = a - b
+Multiplication = a * b
+Division = a / b
 print("Addtion = ", Addition)
 print("Subtraction = ",Subtraction)
+print("Multiplication = ", Multiplication)
+print("Division = ", Division)
