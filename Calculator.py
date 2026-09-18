1c5e292817b58a68386d6bf14fade08fee0047fb
sanjananb803-rgb <sanjananb803@gmail.com>
2026-09-18 15:37:14 +0530

    Add basic calculator operations
    Implement basic calculator functionality for addition and subtraction.

---
+++ Calculator.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the first number:")
+b = int(input("Enter the second number:")
+Addition = a + b
+Subtraction = a - b
+print("Addtion = ", Addition)
+print("Subtraction = ",Subtraction)
